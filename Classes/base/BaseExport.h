a1ce0fded562153592ac229df0b91f34598af72b
xiacg <xiacg@xiacgdembp.lan>
2017-06-20 10:49:31 +0800

    commit

---
+++ Classes/base/BaseExport.h
@@ -0,0 +1,26 @@
+//
+//  BaseExport.h
+//  kgame
+//  Created by  xiacg on 2017/6/20.
+
+#ifndef BaseExport_h
+#define BaseExport_h
+
+#include "base/baseManager/BaseLayer.h"
+#include "base/baseManager/BasePanel.h"
+#include "base/baseManager/BasePanelListView.h"
+#include "base/baseManager/BasePopupPanel.h"
+#include "base/baseManager/BaseScene.h"
+
+#include "base/callback/Callbacks.h"
+
+#include "base/configure/Defination.h"
+#include "base/configure/GameConfig.h"
+#include "base/configure/GameNotificationsAPI.h"
+
+#include "base/macros/GameMacros.h"
+#include "base/macros/DebugMacros.h"
+
+#include "base/tool/GameDataUtils.h"
+
+#endif /* BaseExport_h */
